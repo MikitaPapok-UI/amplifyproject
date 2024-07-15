0f3283461ef6cd2f99b72c22995d1284cb4e1762
papokmikita <mikita.papok@itechart-group.com>
2024-07-15 10:10:08 +0400

    update authentification

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,5 +1,21 @@
 export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "userpool": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "function": {
+    "userpoolCustomMessage": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "zalupa": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
